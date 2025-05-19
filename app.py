48a608c61b5a733768398b240d4146088ca32844
Vishalskk <dushadvishal@gmail.com>
2025-05-19 11:05:26 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import face_recognition
+import os
+
+app = Flask(__name__)
+EVENT_DIR = "event_photos"
+
+@app.route('/find-matches', methods=['POST'])
+def find_matches():
+    user_file = request.files['user_image']
+    event = request.form['event']
+    user_image = face_recognition.load_image_file(user_file)
+    user_enc = face_recognition.face_encodings(user_image)[0]
+    
+    matched = []
+    folder = os.path.join(EVENT_DIR, event)
+    for fname in os.listdir(folder):
+        img_path = os.path.join(folder, fname)
+        img = face_recognition.load_image_file(img_path)
+        encs = face_recognition.face_encodings(img)
+        for enc in encs:
+            if face_recognition.compare_faces([user_enc], enc)[0]:
+                matched.append(fname)
+                break
+    return jsonify({'matched_files': matched})
