5589c924086ada01a3de2cac30a4c2381883d7f8
Vishalskk <108940983+Vishalskk@users.noreply.github.com>
2025-05-19 12:51:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
-from flask import Flask, request, jsonify
-import face_recognition
-import os
+from flask import Flask
 
 app = Flask(__name__)
-EVENT_DIR = "event_photos"
 
-@app.route('/find-matches', methods=['POST'])
-def find_matches():
-    user_file = request.files['user_image']
-    event = request.form['event']
-    user_image = face_recognition.load_image_file(user_file)
-    user_enc = face_recognition.face_encodings(user_image)[0]
-    
-    matched = []
-    folder = os.path.join(EVENT_DIR, event)
-    for fname in os.listdir(folder):
-        img_path = os.path.join(folder, fname)
-        img = face_recognition.load_image_file(img_path)
-        encs = face_recognition.face_encodings(img)
-        for enc in encs:
-            if face_recognition.compare_faces([user_enc], enc)[0]:
-                matched.append(fname)
-                break
-    return jsonify({'matched_files': matched})
-    @app.route('/')
+@app.route('/')
 def home():
     return 'Face backend is running!'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-v
+    app.run(host='0.0.0.0', port=8000)
