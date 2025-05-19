4f11996e9cc4598062615a6afeda6139b4faa14c
Vishalskk <108940983+Vishalskk@users.noreply.github.com>
2025-05-19 12:43:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,6 +23,9 @@ def find_matches():
                 matched.append(fname)
                 break
     return jsonify({'matched_files': matched})
+    @app.route('/')
+def home():
+    return 'Face backend is running!'
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 v
